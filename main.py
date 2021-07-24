b5b9aea38ddf6de422fdd7bfcae25c1066bb6b71
Christopher Milian <christophermilian16@gmail.com>
2021-07-23 23:25:09 -0400

    Updated files for HC and DC Link

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import time
 trigPin = 16
 echoPin = 18
 MAX_DISTANCE = 220  # define the maximum measuring distance, unit: cm
-timeOut = MAX_DISTANCE * 60  # calculate timeout according to the maximum measuring distance
+time_out = MAX_DISTANCE * 60  # calculate timeout according to the maximum measuring distance
+relayPin = 11  # define the relayPin
+debounceTime = 50
 
 
-def pulseIn(pin, level, timeOut):  # obtain pulse time of a pin under timeOut
+def pulse_in(pin, level, time_out):  # obtain pulse time of a pin under timeOut
     t0 = time.time()
-    while (GPIO.input(pin) != level):
-        if ((time.time() - t0) > timeOut * 0.000001):
-            return 0;
+    while GPIO.input(pin) != level:
+        if (time.time() - t0) > time_out * 0.000001:
+            return 0
     t0 = time.time()
-    while (GPIO.input(pin) == level):
-        if ((time.time() - t0) > timeOut * 0.000001):
-            return 0;
-    pulseTime = (time.time() - t0) * 1000000
-    return pulseTime
+    while GPIO.input(pin) == level:
+        if (time.time() - t0) > time_out * 0.000001:
+            return 0
+    pulse_time = (time.time() - t0) * 1000000
+    return pulse_time
 
 
-def getSonar():  # get the measurement results of ultrasonic module,with unit: cm
+def get_sonar():  # get the measurement results of ultrasonic module,with unit: cm
     GPIO.output(trigPin, GPIO.HIGH)  # make trigPin output 10us HIGH level
     time.sleep(0.00001)  # 10us
     GPIO.output(trigPin, GPIO.LOW)  # make trigPin output LOW level
-    pingTime = pulseIn(echoPin, GPIO.HIGH, timeOut)  # read plus time of echoPin
-    distance = pingTime * 340.0 / 2.0 / 10000.0  # calculate distance with sound speed 340m/s
+    ping_time = pulse_in(echoPin, GPIO.HIGH, time_out)  # read plus time of echoPin
+    distance = ping_time * 340.0 / 2.0 / 10000.0  # calculate distance with sound speed 340m/s
     return distance
 
 
@@ -33,13 +35,22 @@ def setup():
     GPIO.setmode(GPIO.BOARD)  # use PHYSICAL GPIO Numbering
     GPIO.setup(trigPin, GPIO.OUT)  # set trigPin to OUTPUT mode
     GPIO.setup(echoPin, GPIO.IN)  # set echoPin to INPUT mode
+    GPIO.setup(relayPin, GPIO.OUT)  # set relayPin to OUTPUT mode
 
 
 def loop():
-    while (True):
-        distance = getSonar()  # get distance
-        print("The distance is : %.2f cm" % (distance))
+    relay_state = True
+    last_change_time = round(time.time() * 1000)
+
+    while True:
         time.sleep(1)
+        distance = get_sonar()  # get distance
+        print("The distance is : %.2f cm" % distance)
+
+        if distance < 5:
+            relay_state = False
+            GPIO.output(relayPin, relay_state)  # If we see something, turn off the relay
+        GPIO.output(relayPin, relay_state)  # If we don't see something, leave the relay on
 
 
 if __name__ == '__main__':  # Program entrance
